9cbc3bb792345e96b85eaa5a0ede2b18e37739bd
Todriya Nikita <nikitatoto9@gmail.com>
2023-09-20 12:24:26 +0300

    fsdagfas

--- js/js.js
+++ js/js.js
@@ -1,10 +1,9 @@
 let select = document.getElementById('create')
 let form = document.getElementById('main_form')
+let output = document.getElementById('main_output')
 
-let autoIncrement = 1;
-let buttonId =1;
-let textId =1;
-let textId2 =1;
+let autoIncrement = 0;
+
 
 select.addEventListener('change', (e) => {
     e.defaultPrevented;
@@ -12,16 +11,26 @@ select.addEventListener('change', (e) => {
     let value = e.target.value;
 
     if (value == 'text') {
+        autoIncrement++
         form.insertAdjacentHTML('beforeend', `
-        <div id="form_block${autoIncrement}">
-            <p>${autoIncrement++}</p>
-            <input type="text" id="text${textId++}">
-            <button id="button_text${buttonId++}"  onclick="addText(document.getElementById('text${textId2++}').value)">Добавить</button>
+        <div id="form_block_${autoIncrement}" class="form_block">
+            <p>${autoIncrement}</p>
+            <input type="text" >
+            <button onclick="addText(${autoIncrement})">Add</button>
+        </div>
         `)
     }
 
     else if (value == 'table') {
-
+        autoIncrement++
+        form.insertAdjacentHTML('beforeend', `
+        <div id="form_block_${autoIncrement}" class="form_block">
+            <p>${autoIncrement}</p>
+            <input type="text" >
+            <button onclick="addTable(${autoIncrement})">Add</button>
+        </div>
+            `
+        )
     }
 
     else {
@@ -32,6 +41,63 @@ select.addEventListener('change', (e) => {
     console.log(value);
 })
 
-function addText(value_text) {
-    console.log(value_text);
+function addText(id) {
+    const div = document.getElementById(`form_block_${id}`)
+    console.log(div.children[1].value);
+    let text = div.children[1].value
+    div.children[2].classList.toggle('hidden')
+    div.insertAdjacentHTML('beforeend', `
+        <button id="change" onclick="changeText(${id})">Change</button>
+        <button id="delete" onclick="deleteBlock(${id})">Delete</button>
+    `);
+    output.insertAdjacentHTML('beforeend', `<p id="text_${id}">${text}</p>`);
+}
+
+function addTable(id) {
+    const div = document.getElementById(`form_block_${id}`)
+    console.log(div.children[1].value);
+    let text = div.children[1].value
+    div.insertAdjacentHTML('beforeend', `
+        <button id="change" onclick="changeTable(${id})">Change</button>
+        <button id="delete" onclick="deleteTable(${id})">Delete</button>
+    `);
+    output.insertAdjacentHTML('beforeend', `
+    <table>
+        <tr>
+            <th>id</th>
+            <th>title</th>
+        </tr>
+        <tr>
+            <td>${id}</td>
+            <td id="title_${id}">${text}</td>
+        </tr>
+    </table>
+    `);
+}
+
+function changeText(id) {
+    const div = document.getElementById(`form_block_${id}`)
+    let newText = div.children[1].value
+    const text = document.getElementById(`text_${id}`)
+    text.innerHTML = newText
+}
+function changeTable(id) {
+    const div = document.getElementById(`form_block_${id}`)
+    let newText = div.children[1].value
+    const text = document.getElementById(`title_${id}`)
+    text.innerHTML = newText
+}
+
+function deleteBlock(id) {
+    const div = document.getElementById(`form_block_${id}`)
+    div.remove()
+    const text = document.getElementById(`text_${id}`)
+    text.remove()
+}
+
+function deleteTable(id) {
+    const div = document.getElementById(`form_block_${id}`)
+    div.remove()
+    const text = document.getElementById(`title_${id}`)
+    text.remove()
 }
